34426d6d3a1e6c38fcaa462be678ddcd4e37fcef
Benjamin Brassart <bbrassar@student.42.fr>
2023-01-30 09:57:10 +0100

    Add function pointer for tests

--- main.c
+++ main.c
@@ -6,25 +6,58 @@
 /*   By: bbrassar <bbrassar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/30 09:10:36 by bbrassar          #+#    #+#             */
-/*   Updated: 2023/01/30 09:22:29 by bbrassar         ###   ########.fr       */
+/*   Updated: 2023/01/30 09:54:37 by bbrassar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libasm.h"
 #include <stdio.h>
 
+#define TEST_FUNCTION(Name) {#Name, __test_ ## Name}
+
+typedef void (*t_test_func)(char const *);
+
 static void __test_ft_strlen(char const *str);
+static void __test_ft_strcpy(char const *str);
+
+typedef struct
+{
+	char const* name;
+	t_test_func func;
+} t_test_decl;
+
+static char const* TEST_VALUES[] = {
+	"hello world!",
+	"!",
+	"",
+	"%s",
+	"123" "\0" "456",
+};
+
+static t_test_decl const TEST_FUNCTIONS[] = {
+	TEST_FUNCTION(ft_strlen),
+	TEST_FUNCTION(ft_strcpy),
+};
 
 int main(void)
 {
-	__test_ft_strlen("hello world!");
-	__test_ft_strlen("!");
-	__test_ft_strlen("");
-	__test_ft_strlen("%s");
-	__test_ft_strlen("123" "\0" "456");
+	for (size_t i = 0; i < (sizeof TEST_FUNCTIONS / sizeof (*TEST_FUNCTIONS)); ++i)
+	{
+		printf("\n-- Testing %s --\n\n", TEST_FUNCTIONS[i].name);
+		for (size_t j = 0; j < (sizeof TEST_VALUES / sizeof (*TEST_VALUES)); ++j)
+			TEST_FUNCTIONS[i].func(TEST_VALUES[j]);
+	}
 }
 
 static void __test_ft_strlen(char const *str)
 {
 	printf("ft_strlen(\"%s\") = %lu\n", str, ft_strlen(str));
+}
+
+static void __test_ft_strcpy(char const *str)
+{
+	char buffer[2048] = "this is the original buffer";
+	char* res = ft_strcpy(buffer, str);
+
+	printf("'%s' (addr match: %s)\n", res, (buffer == res) ? "true" : "false");
 }
