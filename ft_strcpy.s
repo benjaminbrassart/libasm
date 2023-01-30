964cd87dc4e130cbc7c83052f83b571c931338d6
Benjamin Brassart <bbrassar@student.42.fr>
2023-01-30 10:44:46 +0100

    Fix ft_strcpy byte-by-byte copy

--- ft_strcpy.s
+++ ft_strcpy.s
@@ -8,21 +8,21 @@ global ft_strcpy
 ; return = rax
 ;
 ft_strcpy:
-    push rdi            ; save `dst` to the stack so we can return it later
+    push rdi                    ; save `dst` to the stack so we can return it later
     jmp .loop
 
 .loop:
-    mov rax, [rsi]      ; tmp = *src
-    mov [rdi], rax      ; *dst = tmp
+    mov al, byte [rsi]          ; tmp = *src
+    mov byte [rdi], al          ; *dst = tmp
 
-    cmp byte [rsi], 0   ; *src == 0
+    cmp byte [rsi], 0           ; *src == 0
     je .end
 
-    inc rdi             ; ++dst
-    inc rsi             ; ++src
+    inc rdi                     ; ++dst
+    inc rsi                     ; ++src
 
     jmp .loop
 
 .end:
-    pop rax             ; get `dst` back from the stack and put it in the return value
+    pop rax                     ; get `dst` back from the stack and put it in the return value
     ret
