f3f20e0562a4ba06124bde2a310419aab9016e59
Benjamin BRASSART <bbrassar@student.42.fr>
2023-02-06 13:34:01 +0100

    Fix ft_list_size always returning 0

--- bonus/ft_list_size.s
+++ bonus/ft_list_size.s
@@ -8,14 +8,16 @@ global ft_list_size
 ;
 ft_list_size:
     mov rax, 0                  ; initialize return value to 0
+    jmp .loop
 
+.loop:
     cmp rdi, 0                  ; begin == NULL
     je .end
 
     inc rax                     ; increment return value
 
     mov rdi, [rdi + 8]          ; begin = begin->next
-    jmp ft_list_size
+    jmp .loop
 
 .end:
     ret
