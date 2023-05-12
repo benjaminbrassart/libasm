d371ca1143c02b7b51708660343399119f505c3a
Benjamin Brassart <bbrassar@student.42.fr>
2023-05-12 04:28:41 +0200

    Get rid of ugly invalid character check Implement duplicated character base check

--- bonus/ft_atoi_base.s
+++ bonus/ft_atoi_base.s
@@ -23,42 +23,56 @@ ft_atoi_base:
     cmp rax, 2
     jl .error
 
+    ; save base for later
     push rsi
     jmp .check_base_chars
 
-;
-; TODO
-;
-; this is ugly as fuck, maybe try to
-; implement a jump table like this
-;
-; https://www.reddit.com/r/learnprogramming/comments/hgm5g8/x8664_switch_jump_table_assembly_implementation/
-;
 .check_base_chars:
-    cmp byte [rsi], 0x2b        ; 0x2b -> '+'
-    je .error
-    cmp byte [rsi], 0x2d        ; 0x2d -> '-'
-    je .error
-    cmp byte [rsi], 0x09        ; 0x09 -> '\t'
-    je .error
-    cmp byte [rsi], 0x0a        ; 0x0a -> '\n'
-    je .error
-    cmp byte [rsi], 0x0b        ; 0x0b -> '\v'
-    je .error
-    cmp byte [rsi], 0x0c        ; 0x0c -> '\f'
-    je .error
-    cmp byte [rsi], 0x0d        ; 0x0d -> '\r'
-    je .error
-    cmp byte [rsi], 0x20        ; 0x20 -> ' '
-    je .error
-
+    ; if *base == '\0', base characters are ok. now check duplicated characters
     cmp byte [rsi], 0x00
     je .check_dup_chars
 
+    ; *base <= ' ' -> error
+    cmp byte [rsi], 0x20
+    jle .error
+    ; *base == 0x7f (DEL) -> error
+    cmp byte [rsi], 0x7f
+    je .error
+
+    ; check next character in base
     inc rsi
     jmp .check_base_chars
 
 .check_dup_chars:
+    ; restore base
+    pop rsi
+    jmp .check_dup_chars_loop
+
+.check_dup_chars_loop:
+    ; if *base == '\0', goto atoi_base
+    cmp byte [rsi], 0
+    je .atoi_base
+
+    mov rcx, rsi
+    inc rcx
+    jmp .check_dup_chars_loop_inner
+
+.check_dup_chars_inc_base:
+    inc rsi
+    jmp .check_dup_chars_loop
+
+.check_dup_chars_loop_inner:
+    cmp byte [rcx], 0
+    je .check_dup_chars_inc_base
+    mov al, [rcx]
+    cmp byte [rsi], al
+    je .error
+    inc rcx
+    jmp .check_dup_chars_loop_inner
+
+.atoi_base:
+    mov rax, 1 ; TODO
+    ret
 
 .error:
     mov rax, 0
