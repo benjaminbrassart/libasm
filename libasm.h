1d075c3e1fd711697ceac1b49632e4b78beb7ad7
Benjamin Brassart <bbrassar@student.42.fr>
2023-05-22 15:42:36 +0200

    Add missing prototype for ft_read

--- libasm.h
+++ libasm.h
@@ -6,7 +6,7 @@
 /*   By: bbrassar <bbrassar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/30 09:13:01 by bbrassar          #+#    #+#             */
-/*   Updated: 2023/01/30 11:50:49 by bbrassar         ###   ########.fr       */
+/*   Updated: 2023/05/22 15:36:31 by bbrassar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,10 +15,22 @@
 
 # include <stddef.h>
 
-size_t	ft_strlen(char const *str);
-char	*ft_strcpy(char *dst, char const *src);
-char	*ft_strdup(char const *str);
-int		ft_strcmp(char const* s1, char const* s2);
-int     ft_write(int fd, void const* buffer, size_t len);
+size_t
+ft_strlen(char const *str);
+
+char*
+ft_strcpy(char *dst, char const *src);
+
+char*
+ft_strdup(char const *str);
+
+int
+ft_strcmp(char const* s1, char const* s2);
+
+int
+ft_write(int fd, void const* buffer, size_t len);
+
+int
+ft_read(int fd, void* buffer, size_t len);
 
 #endif
